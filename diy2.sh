fed24a174cd1afe3028fe7f2150572c03131956b
tianxiaweiyidao <80669685+tianxiaweiyidao@users.noreply.github.com>
2023-10-15 12:03:15 +0800

    Update diy2.sh
    修改ip地址

--- diy2.sh
+++ diy2.sh
@@ -20,6 +20,6 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.101.2/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By 001 $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
